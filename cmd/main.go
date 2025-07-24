2681e36c6eea81fc7de865aeef625aab850247f4
DarjaLeonova <darja.leonova.it@gmail.com>
2025-07-24 17:53:57 +0300

    Initialized router/server/db connection

--- cmd/main.go
+++ cmd/main.go
@@ -1,21 +1,48 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/swaggo/http-swagger"
+	"log"
+	"moneyTransfer/api/handler"
+	"moneyTransfer/internal/domain/service"
+	"moneyTransfer/internal/repository"
+	"moneyTransfer/internal/repository/postgres"
+	"net/http"
+
+	_ "moneyTransfer/docs"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
+// @title Money Transfer API
+// @version 1.0
+// @description REST API for transferring money between users
+// @host localhost:8080
+// @BasePath /
 
 func main() {
-	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-	s := "gopher"
-	fmt.Println("Hello and welcome, %s!", s)
-
-	for i := 1; i <= 5; i++ {
-		//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-		fmt.Println("i =", 100/i)
+	db, err := postgres.NewPostgresClient()
+	if err != nil {
+		log.Fatal("failed to connect to database:", err)
 	}
+	defer db.Close()
+
+	var transferRepo repository.TransferRepository = postgres.NewTransferRepository(db)
+	var userRepo repository.UserRepository = postgres.NewUserRepository(db)
+
+	transferService := service.NewTransferService(transferRepo, userRepo)
+	userService := service.NewUserService(userRepo)
+
+	transferController := handler.NewTransferController(transferService)
+	userController := handler.NewUserController(userService)
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/transfers/{userId}", transferController.GetTransactionsByUserID).Methods("GET")
+	router.HandleFunc("/transfers", transferController.CreateTransaction).Methods("POST")
+	router.HandleFunc("/balance/{userId}", userController.GetUserBalance).Methods("GET")
+
+	router.PathPrefix("/swagger/").Handler(httpSwagger.WrapHandler)
+
+	log.Println("Server listening on :8080")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
